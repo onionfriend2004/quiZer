cae1945c1105a9660e7ca92b8798eed9969a7d98
Alexey Lupenkov <alupenkov@inbox.ru>
2024-06-22 23:35:22 +0300

    Created login and register templates

--- quiZer/main/views.py
+++ quiZer/main/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def login(request):
+    return render(request, 'main/registration/login.html')
+
+def sign_up(request):
+    return render(request, 'main/registration/sign_up.html')
 # Create your views here.
--- quiZer/quiZer/settings.py
+++ quiZer/quiZer/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
     'crispy_forms',
     'crispy_bootstrap5',
+    'main.apps.MainConfig',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
